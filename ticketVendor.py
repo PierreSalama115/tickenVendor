25767beb9eb2264bf1f3e3026d809ad991a166ed
PierreSalama115 <146039486+PierreSalama115@users.noreply.github.com>
2023-11-08 12:24:50 -0500

    Add files via upload

---
+++ ticketVendor.py
@@ -0,0 +1,231 @@
+import os
+from datetime import datetime, timedelta
+import ticketVendorBackEnd 
+
+class User:
+    """
+    In the User class the program takes in the the strings username,
+    password and checks if the user that logged in is an admin
+    """
+
+    def __init__(self, username: str, password: str, is_admin: bool = False):
+        """
+        (User, str, str, bool) -> None
+        this does somerjj
+        """
+        self.username = username
+        self.password = password
+        self.is_admin = is_admin
+
+
+class System:
+    """
+    This class stores the users logged in an array called users cand checks the current user.
+    there are methods in this class which gives the user options to what the want to do to contine with the program.
+    For example when the user enters the option L the System will output the corresponding output the the option L.
+
+    """
+
+    # This method has an array to store the users logged in and the current_user is None and is_logged_in is false because  no one is logged in yet
+    def __init__(self):
+        self.users = []
+        self.current_user = None
+        self.is_logged_in = (
+            False  # New variable to track login state. This variable is a boolean
+        )
+
+    # Adding User to the users array
+    def addUser(self, user):
+        self.users.append(user)
+
+    # This is the login method where the user will enter their username and password to log in
+
+    def optionL(self):
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        if ticketVendorBackEnd.login(username, password):
+            # Login successful
+            self.current_user = username
+            self.is_logged_in = True
+            print(f"****Currently logged in as {username} ****")
+        else:
+            # Login failed
+            print("Invalid credentials. Please try again.")
+
+    # In the method optionL it logs in the user
+    
+
+    # In the method optionN it allows  the user to create a new account
+    def optionN(self):
+        ticketVendorBackEnd.createNewAccount()
+
+    # In the method option1 it allows the user to Add Credit to their account
+    def option1(self):
+        if self.is_logged_in:  # Check the login state variable
+            print("You chose to Add Credit.")
+            amount = input("Enter the credit amount (not over 1000$):  \n")
+            if amount.isdigit() and int(amount) <= 1000:
+                credit_card = input("Enter your credit card number (16digits-MM/YY-CVV):  \n")
+                if len(credit_card) == 23:
+                    # Call the backend function to add credit
+                    ticketVendorBackEnd.defAddCredit(self.current_user.username, int(amount))
+                    print("You have successfully added credit to your account. Press Enter to continue...")
+                else:
+                    print("Credit Card number should be (16digits-MM/YY-CVV). Returning to main menu...")
+            else:
+                print("Invalid credit amount. Returning to main menu.")
+        else:
+            print("You need to be logged in to access this option.")
+        input("Press Enter to continue...  \n")
+
+    # In the method option2 it allows the user to create an event from tomorrow's date to 2 years from today if the user is an admin
+    def option2(self):
+        if self.is_logged_in:  # Check the login state variable
+            if self.current_user.is_admin:  # Check if the user is an admin
+                print("You selected 'Create Event'.")
+                event_name = input("Enter the event name (15 characters at most):  \n")
+                if len(event_name) <= 15:
+                    event_date = input(
+                        "Enter the event date (YYYYMMDD) from tomorrow to two years from today: \n "
+                    )
+                    if len(event_date) == 8 and event_date.isdigit():
+                        current_date = datetime.now()
+                        tomorrow = current_date + timedelta(days=1)
+                        two_years_from_today = current_date + timedelta(days=730)
+
+                        event_date_obj = datetime.strptime(event_date, "%Y%m%d")
+
+                        if tomorrow <= event_date_obj <= two_years_from_today:
+                            tickets = input(
+                                "Enter the number of tickets to allocate for the event (at most 9999):  \n"
+                            )
+                            if tickets.isdigit() and 0 < int(tickets) <= 9999:
+                                print("Press Enter to continue...")
+                            else:
+                                print(
+                                    "Invalid ticket number. Should be a positive integer at most 9999. Returning to the main menu."
+                                )
+                        else:
+                            print(
+                                "Event date should be from tomorrow to two years from today. Returning to the main menu."
+                            )
+                    else:
+                        print(
+                            "Invalid date format. Event date should be in YYYYMMDD format. Returning to the main menu."
+                        )
+                else:
+                    input("Successful...Press Enter to continue...  \n")
+            else:
+                print("You need to be an admin to create an event")
+                input("Press Enter to continue... \n")
+        else:
+            print("You need to be logged in to access this option.")
+            input("Press Enter to continue... \n")
+
+    # The method option3 is to add tickets to an existing event and the user needs to be an admin to access this option
+    def option3(self):
+        if self.is_logged_in:
+            if self.current_user.is_admin:  # Check the login state variable
+                event_name = input("Enter the event name:  \n")
+                tickets = input("Enter the number of tickets:  \n")
+                # Implement logic for adding tickets to an existing event
+                print("Press Enter to continue...")
+            else:
+                print("You need to be an admin to access this option.")
+
+    # The method option4 is to cancel tickets and delete any existing events, and the user needs to be an admin to access this option
+    def option4(self):
+        if (
+            self.is_logged_in and self.current_user.is_admin
+        ):  # Check the login state variable
+            if self.current_user.is_admin:
+                event_name = input("Enter the event name:  \n")
+                # Implement logic for canceling tickets and deleting an event
+                print("Press Enter to continue...")
+            else:
+                print("You need to be an admin to access this option.")
+
+    # This method option5 is to sell a number of tickets for an event and enter the event name and the number of tickets
+    def option5(self):
+        if self.is_logged_in:  # Check the login state variable
+            event_name = input("Enter the event name:  \n")
+            ticket_number = input("Enter a ticket number:  \n")
+            # Implement logic for selling tickets for an existing event
+            print("Press Enter to continue...")
+
+    # The method option6 is to return tickets for any existing events, and the user needs to be an admin to access this option
+    def option6(self):
+        if self.is_logged_in:  # Check the login state variable
+            if self.current_user.is_admin:
+                event_name = input("Enter the event name: \n ")
+                ticket_number = input("Enter a ticket number: \n ")
+                # Implement logic for returning tickets of an existing event
+                print("You returned the ticket. Press Enter to continue.")
+            else:
+                print("You need to be an admin to access this option.")
+
+    # The method option7 is to log out of the system
+    def option7(self):
+        self.current_user = None
+        self.is_logged_in = False  # Set login state to False
+        print("Logged out successfully. Press Enter to continue.")
+
+    #The method option8 is to exit the program,this was an orginal requirment
+    def optionQ(self):
+        ticketVendorBackEnd.exitProgram()  # Save databases and reset state
+        self.current_user = None
+        self.is_logged_in = False
+        print("saving to database...\n" + "***TERMINATE PROGRAM****")
+        exit()
+
+    # This is the main menu that will be displayed when the program is run and the user picks there choice
+    def main_menu(self):
+        while True:
+            print("L. Login")
+            print("N. Create New Account")
+            print("1. Add Credit")
+            print("2. Create Event")
+            print("3. Add")
+            print("4. Delete")
+            print("5. Sell")
+            print("6. Return")
+            print("7. Logout")
+            print("Q. Exit")
+            choice = input("Enter your choice: \n")
+
+            # The options will be matched to its corresponding method
+            action_mapper = {
+                "L": self.optionL,
+                "N": self.optionN,
+                "1": self.option1,
+                "2": self.option2,
+                "3": self.option3,
+                "4": self.option4,
+                "5": self.option5,
+                "6": self.option6,
+                "7": self.option7,
+                "Q": self.optionQ,
+            }
+
+            action = action_mapper.get(choice.upper(), None)
+
+            if action:
+                if (
+                    choice.upper() in ["1", "2", "3", "4", "5", "6", "7"]
+                    and not self.is_logged_in
+                ):
+                    print("You need to be logged in to access this option.")
+                else:
+                    action()
+            else:
+                print("Invalid choice. Please enter a valid option.")
+
+
+# This is the main where we pass in the parameter values for the User class
+if __name__ == "__main__":
+    system = System()
+    system.addUser(User("admin", "admin", is_admin=True))
+    system.addUser(User("agent", "pass", is_admin=False))
+
+    system.main_menu()
---
+++ ticketVendorBackEnd.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import os
+
+# Define column names in camelCase
+userColumns = [
+    "nameOfPerson",
+    "ticketNumber",
+    "eventName",
+    "username",
+    "password",
+    "accountType",
+    "transactionNumber",
+    "creditOnFile"
+]
+ticketColumns = ["ticketNumber", "price", "remainingTickets"]
+eventColumns = ["eventName", "remainingTickets"]
+
+# Path to the backend data directory
+dataDirPath = "backendData"
+
+# Ensure that the data directory exists
+os.makedirs(dataDirPath, exist_ok=True)
+
+# Function to load DataFrame from a text file or create a new one if file does not exist
+def loadOrInitializeDf(fileName, columns, sep='\t'):
+    filePath = os.path.join(dataDirPath, fileName)
+    if os.path.exists(filePath):
+        return pd.read_csv(filePath, sep=sep)
+    else:
+        df = pd.DataFrame(columns=columns)
+        df.to_csv(filePath, sep=sep, index=False)
+        return df
+
+
+
+# File names
+userFileName = "users.txt"
+ticketFileName = "tickets.txt"
+eventFileName = "events.txt"
+
+# Load or create DataFrames
+users = loadOrInitializeDf(userFileName, userColumns)
+tickets = loadOrInitializeDf(ticketFileName, ticketColumns)
+events = loadOrInitializeDf(eventFileName, eventColumns)
+
+def login(username, password):
+    if username.lower() in ['agent', 'admin'] and password.lower() in ['agent', 'admin']:
+        # Login successful
+        return True
+    else:
+        # Login failed
+        return False
+
+def defAddCredit(username):
+    global users
+
+    # Ensure the user exists
+    if username not in users['username'].values:
+        print("User not found.")
+        return
+
+    try:
+        # Prompt for the credit amount
+        amount = float(input("Enter the credit amount to add: $"))
+        if amount <= 0:
+            print("Invalid amount. Please enter a positive number.")
+            return
+
+        # Update the user's credit in the DataFrame
+        users.loc[users['username'] == username, 'creditOnFile'] += amount
+
+        print(f"${amount} credit added successfully to {username}'s account.")
+
+        # Optionally, save the updated DataFrame to the file
+        users.to_csv(os.path.join(dataDirPath, userFileName), sep='\t', index=False)
+    except ValueError:
+        print("Invalid input. Please enter a numerical value.")
+
+
+
+def createNewAccount():
+    global users
+
+    username = input("Enter new username or type 'EXIT' to cancel: ").strip()
+    if username.lower() == 'exit':
+        return
+
+    if username.lower() in ['agent', 'admin']:
+        print("This username is reserved. Please choose a different one.")
+        createNewAccount()  # Recursive call
+        return
+
+    # Check if username already exists in users DataFrame
+    if not users[users['username'] == username].empty:
+        print("This username already exists. Please choose a different one.")
+        createNewAccount()  # Recursive call
+        return
+
+    # Proceed with new account creation
+    nameOfPerson = input("Enter your name: ")
+    password = input("Enter your password: ")
+    accountType = "agent"
+    transactionNumber = 0
+    creditOnFile = 0
+
+    # Adding new user to DataFrame
+    newUser = {
+        "nameOfPerson": nameOfPerson,
+        "ticketNumber": 0,
+        "eventName": "",
+        "username": username,
+        "password": password,
+        "accountType": accountType,
+        "transactionNumber": transactionNumber,
+        "creditOnFile": creditOnFile
+    }
+
+    users = users.append(newUser, ignore_index=True)
+    
+    print(f"User {username} created successfully.")
+
+
+
+
+def exitProgram():
+    global users, tickets, events
+
+    # Save the current state of DataFrames to text files
+    users.to_csv(os.path.join(dataDirPath, userFileName), sep='\t', index=False)
+    tickets.to_csv(os.path.join(dataDirPath, ticketFileName), sep='\t', index=False)
+    events.to_csv(os.path.join(dataDirPath, eventFileName), sep='\t', index=False)
+    print("Databases saved successfully.")
